17748b85e137cdb6e77ac24f1c1a573943691686
Isaiah Heavens <isaiahheavens@Isaiahs-MacBook-Pro.local>
2019-11-15 02:38:59 -0500

    adding color

--- gameFunctions/compare.py
+++ gameFunctions/compare.py
@@ -5,7 +5,7 @@ from gameFunctions import gameVars
 def compareChoices(player):
 	if  player == gameVars.computer:
 
-		print("tie, no one wins! try again")
+		print("\033[1;36;40mtie, no one wins! try again.")
 	
 	elif player == "quit":
 		print("you chose to quit, quitter.")
@@ -13,24 +13,24 @@ def compareChoices(player):
 	
 	elif player == "rock":
 		if gameVars.computer == "paper":
-			print("You Lose!", gameVars.computer, "covers", player, "\n")
+			print("\033[1;31;40mYou Lose!", gameVars.computer, "covers", player, "\n")
 			gameVars.player_lives = gameVars.player_lives -1
 		else:
-			print("You Won!", player, "smashes", gameVars.computer, "\n")
+			print("\033[1;32;40mYou Won!", player, "smashes", gameVars.computer, "\n")
 			gameVars.computer_lives = gameVars.computer_lives -1
 
 	elif player == "paper":
 		if gameVars.computer == "scissors":
-			print("You Lose!", gameVars.computer, "cuts", player, "\n")
+			print("1;31;40mYou Lose!", gameVars.computer, "cuts", player, "\n")
 			gameVars.player_lives = gameVars.player_lives -1
 		else:
-			print("You Won!", player, "covers", gameVars.computer, "\n")
+			print("\033[1;32;40mYou Won!", player, "covers", gameVars.computer, "\n")
 			gameVars.computer_lives = gameVars.computer_lives -1
 
 	elif player == "scissors":
 		if gameVars.computer == "rock":
-			print("You Lose!", gameVars.computer, "smashes", player, "\n")
+			print("1;31;40mYou Lose!", gameVars.computer, "smashes", player, "\n")
 			gameVars.player_lives = gameVars.player_lives -1
 		else:
-			print("You Won!", player, "cuts", gameVars.computer, "\n")
+			print("\033[1;32;40mYou Won!", player, "cuts", gameVars.computer, "\n")
 			gameVars.computer_lives = gameVars.computer_lives -1
